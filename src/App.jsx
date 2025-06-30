15d31a80247d177d579e92e33c03e86a9aff2c0f
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-06-29 22:36:43 -0300

    agregue hoja de estilos en rositas

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ function App() {
 
   return (
     <>
-      
+      <main className="fondoRosaClaro">
+      <h1 className="text-center">Formulario</h1>
+      </main>
+      <footer className="fondoRosaOscuro text-center py-3">
+        <p>&copy; Todos los derechos reservados - Claudia Ximena Barrientos</p>
+      </footer>
     </>
   )
 }
